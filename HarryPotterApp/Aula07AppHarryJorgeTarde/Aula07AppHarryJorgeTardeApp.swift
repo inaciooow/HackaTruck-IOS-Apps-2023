c50a898fff5877707821e46d83bbb41687117540
Jorge Inácio <103686150+inaciooow@users.noreply.github.com>
2026-04-17 15:06:46 -0300

    Add files via upload

---
+++ HarryPotterApp/Aula07AppHarryJorgeTarde/Aula07AppHarryJorgeTardeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Aula07AppHarryJorgeTardeApp.swift
+//  Aula07AppHarryJorgeTarde
+//
+//  Created by Student16 on 25/05/23.
+//
+
+import SwiftUI
+
+@main
+struct Aula07AppHarryJorgeTardeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
